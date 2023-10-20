87b20d89930a11a4154aa463c5d6d903fdc7e89d
SHUBHAM SINGH <shubham.0202.in@gmail.com>
2023-10-20 09:25:14 +0530

    changing the content of this repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# SINGH-ASSOCIATION
-First Website
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./dist/index.html"],
+    content: ["*.html"],
     theme: {
         extend: {},
     },
