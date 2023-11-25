b7ffcec066f08fa5046aded830004ab4175ab822
SHUBHAM SINGH <shubham.0202.in@gmail.com>
2023-11-25 23:22:15 +0530

    ok

---
+++ script.js
@@ -0,0 +1,34 @@
+movies = [{
+        name: 'Avenger',
+        rating: 'IMD:7.9',
+        poster: "https://upload.wikimedia.org/wikipedia/en/b/b6/Lagaan.jpg",
+    },
+
+
+    {
+        name: 'Lagan',
+        rating: 'IMD:8.2',
+        poster: "https://upload.wikimedia.org/wikipedia/en/b/b6/Lagaan.jpg",
+    },
+
+    {
+        name: "Jhon Wick",
+        rating: 'IMD:6.4',
+        poster: "https://www.tallengestore.com/cdn/shop/products/JohnWick-KeanuReeves-HollywoodEnglishActionMoviePoster-2_c927bf3a-7e64-4c67-b05f-960c2f2cd3d0.jpg?v=1649071611",
+    },
+
+
+    {
+        name: 'Black Panther',
+        rating: "IMD:4.8",
+        poster: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/99790a53131197.596c4c8d36869.jpg",
+    },
+
+    {
+        name: 'Leo',
+        rating: 'IMD:10.4',
+        poster: "https://rukminim2.flixcart.com/image/850/1000/xif0q/poster/h/q/w/small-vijay-thalapathy-poster-300-gsm-12x18-unframed-rfcp-719-original-imagsgmkjrepjsfh.jpeg?q=20",
+    }
+
+
+]
