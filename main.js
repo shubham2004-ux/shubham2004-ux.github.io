010ce6037f0f1b1cb8051d72c7b35e951703112e
shubham <shubham.0202.in>
2024-01-23 22:53:40 +0530

    d
    ok

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 let serviceProvide = [{
-        logo: "fa-solid fa-laptop",
+        logo: "sync_saved_locally",
         Tradition: 'Web Devlopment',
         provide: 'Web page| Hosting | Rest Api Build'
 
 
     },
     {
-        logo: 'fa-brands fa-app-store-ios',
+        logo: 'deployed_code',
         Tradition: 'App Devlopment',
         provide: 'App Services | Any Type App Creation | Rest Api Creation'
     },
     {
-        logo: "fa-brands fa-google",
+        logo: "search",
         Tradition: 'SEO',
         provide: 'Top Site Ranking | Enhance The Rank '
     },
     {
-        logo: "fa-solid fa-database",
+        logo: "captive_portal",
 
         Tradition: 'Data Anylisis',
         provide: 'Any Type Of  BigData | Use Special Keyword  '
@@ -26,15 +26,19 @@ let serviceProvide = [{
 function Servicey() {
     let Service = `  `
     for (let i = 0; i < serviceProvide.length; i++) {
-        Service = Service + `<div style="box-shadow: -8px -8px 10px; " class=" text-[15px]  sm:text-[1.4vw] bg-white rounded-xl m-5  md:w-[20%] overflow-hidden h-[90%]">
-        <i style="align-self: center;" class=" mt-[5%] mx-[40%]  ${serviceProvide[i].logo}  text-[200%]"></i>
-
-
-
-        <p class="text-[10%] mt-[5%] mx-[5%]">${serviceProvide[i].Tradition}</p>
-        <p class="text-[80%] mx-[5%]">${serviceProvide[i].provide}</p>
-        <p class="text-[60%]  mx-[5%]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil amet nisi doloremque impedit sequi consectetur cupiditate dolorum </p>
-        <button class="bg-purple-600  text-white rounded-xl p-[7px] mx-[5%]">View More</button>
+        Service = Service + `<div style="box-shadow: -8px -8px 10px; " class=" flex flex-col m-2  items-center text-[15px]  sm:text-[1.4vw] bg-white rounded-xl  md:w-[20%]  h-[90%]">
+         <div class="m-2 flex-col flex items-center justify-center">
+         <span class="material-symbols-outlined  ">
+         ${serviceProvide[i].logo}
+    </span>
+    
+    
+    
+            <p class="">${serviceProvide[i].Tradition}</p>
+            <p class="">${serviceProvide[i].provide}</p>
+            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil amet nisi doloremque impedit sequi consectetur cupiditate dolorum </p>
+            <button class="">View More</button>
+         </div>
 
 
         </div>`
@@ -81,7 +85,7 @@ document.querySelector(".netflix-live").addEventListener("click", () => {
 })
 
 document.querySelector(".git-live").addEventListener("click", () => {
-    location.href = "https://github.io"
+    location.href = "https://github.com/ssdevloper/Main-Project/tree/master/SIGMA%20NETFLIX"
 })
 
 
